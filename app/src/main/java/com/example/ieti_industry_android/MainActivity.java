b66c9c22fbab1236613c99fe6c4698e8c9a9fa23
eriktamano <eriktamano@gmail.com>
2022-11-10 17:03:01 +0100

    Interficie login creada

--- app/src/main/java/com/example/ieti_industry_android/MainActivity.java
+++ app/src/main/java/com/example/ieti_industry_android/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.ieti_industry_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button loginButton = findViewById(R.id.loginButton);
+        final EditText server = findViewById(R.id.Server);
+        final EditText user = findViewById(R.id.User);
+        final EditText password = findViewById(R.id.Password);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("SERVER", server.getText().toString());
+                Log.i("USER", user.getText().toString());
+                Log.i("PASS", password.getText().toString());
+            }
+        });
     }
+
+
+
 }
